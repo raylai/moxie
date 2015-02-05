81a9f4d9af60d4b7ef3e8ffbba5d96fd7810d14f
Ray Lai <ray@raylai.com>
2015-02-05 11:26:08 +0800

    Log date and time as well.

--- moxie.go
+++ moxie.go
@@ -307,7 +307,7 @@ func lookup(url string) (*mega.Node, error) {
 }
 
 func main() {
-	log.SetFlags(log.Lshortfile)
+	log.SetFlags(log.Ldate|log.Ltime|log.Lshortfile)
 	user := os.Getenv("MEGA_USER")
 	pass := os.Getenv("MEGA_PASSWD")
 	megaSession = mega.New()
